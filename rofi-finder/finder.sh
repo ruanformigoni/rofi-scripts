f4b20dc46f5292a2a61453c0e1427c7559f9bd05
Ruan <formigoni@outlook.com>
2019-01-03 06:48:39 -0200

    finder.sh refactored

--- rofi-finder/finder.sh
+++ rofi-finder/finder.sh
@@ -1,39 +1,48 @@
 #!/usr/bin/env bash
 
 #PUT THIS FILE IN ~/.local/share/rofi/finder.sh
-#USE: rofi  -show find -modi find:~/.local/share/rofi/finder.sh
+#USE: rofi	-show find -modi find:~/.local/share/rofi/finder.sh
+baseDir="$HOME"
+
+search_opt(){
+	echo -e "! \uf002 Type your search query to find files"
+	echo -e "! \uf0f0 You can refine the results typing again"
+	echo -e "! \ue7a1 Type ?<string> to ignore the results and make another search"
+	echo -e "! \uf1d6 Type ! or nothing to show this message again"
+}
+
+search_empty(){
+	if [[ ! -z "$1" ]]; then
+		echo "$1"
+	else
+		echo -e "! \uf872 Oh noes! Nothing found'"
+		search_opt
+	fi
+}
+
+search(){
+		result=$(locate -i --regexp $1 | sed -e '/\..*\// d' 2>&1)
+		search_empty "$result"
+	
+}
+
 if [ ! -z "$@" ]
 then
-  QUERY=$@
-  if [[ "$@" == /* ]]
-  then
-    if [[ "$@" == *\?\? ]]
-    then
-      coproc ( exo-open "${QUERY%\/* \?\?}"  > /dev/null 2>&1 )
-      exec 1>&-
-      exit;
-    else
-      coproc ( exo-open "$@"  > /dev/null 2>&1 )
-      exec 1>&-
-      exit;
-    fi
-  elif [[ "$@" == \!\!* ]]
-  then
-    echo "!!-- Type your search query to find files"
-    echo "!!-- To search again type !<search_query>"
-    echo "!!-- To search parent directories type ?<search_query>"
-    echo "!!-- You can print this help by typing !!"
-  elif [[ "$@" == \?* ]]
-  then
-    while read -r line; do
-      echo "$line" \?\?
-    done <<< $(find / -iname *"${QUERY#\?}"* 2>&1 | grep -v 'Permission denied\|Input/output error')
-  else
-    find / -iname *"${QUERY#!}"* 2>&1 | grep -v 'Permission denied\|Input/output error'
-  fi
+	QUERY=$@
+	if [[ "$@" == /* ]]
+	then
+		coproc ( exo-open "$@"	> /dev/null 2>&1 )
+		exec 1>&-
+		exit;
+	elif [[ "$@" = \!* ]]
+	then
+		search_opt
+	elif [[ "$@" = \?* ]]
+	then
+		search "$baseDir.*${QUERY#?}.*" 
+	else
+		search "$baseDir.*${QUERY#!}.*";
+	fi
 else
-  echo "!!-- Type your search query to find files"
-  echo "!!-- To seach again type !<search_query>"
-  echo "!!-- To seach parent directories type ?<search_query>"
-  echo "!!-- You can print this help by typing !!"
+	search_opt
 fi
